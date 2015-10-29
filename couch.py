8d960acb2d811b3a481b6b8b746ec16e2bd5277c
Ben Johnson <b@groundfoundation.org>
2015-10-29 19:47:23 -0400

    Added DB adds and Searches

--- couch.py
+++ couch.py
@@ -1,5 +1,5 @@
 import couchdb
-from uuid import uuid4
+import datetime
 
 
 def connectSRV(srv = 'http://localhost:5984'):
@@ -33,6 +33,7 @@ def view(db,s):
 def viewDB(db):
   for o in db:
     #print("\tID = {} -- {}".format(db[o]['_id'],db[o]['_rev']))
+    print("  ID = {}".format(db[o]['_id']))
     for k in db[o]:
       if not (k == '_id' or k == '_rev'):
         print("\t{} : {}".format(k,db[o][k]))
@@ -44,3 +45,81 @@ def viewAll(c):
       db = c[o]
       viewDB(db)
 
+def now():
+  now = datetime.datetime.now()
+  return now
+
+def getDocItems(dType):
+  r = []
+  if dType.lower() == 'venue':
+    r = [
+      'name',
+      'address1',
+      'address2',
+      'city',
+      'state',
+      'hashtag',
+      'website',
+      'gps',
+      ]
+  elif dType.lower() == 'artist':
+    r = [
+      'name',
+      'info',
+      'website',
+      'hashtag',
+       ]
+  elif dType.lower() == 'region':
+    r = [
+      'gps_n',
+      'gps_s',
+      'gps_e',
+      'gps_w',
+      ]
+  elif dType.lower() == 'crawler':
+    r = [
+      'site',
+      'taglist',
+       ]
+  elif dType.lower() == 'agegroup':
+    r = [
+      'name',
+      'info',
+       ]
+
+  return r
+
+def add(dType,name):
+  keys = getDocItems(dType)
+  dic = {'type' : dType}
+  print('Adding {} type Doc: {}'.format(dType,name))
+  for i in keys:
+    if i == 'name':
+     dic[i] = name 
+    elif i == 'taglist':
+      l = []
+      t = raw_input("Type a search tag: ")
+      while (t != ''):
+        l.append(t)
+        t = raw_input("Type a search tag: ")
+      if len(l) > 0: dic[i] = l
+    else:
+      t = raw_input("Type {} {}: ".format(dType,i))
+      if t != '': dic[i] = t
+  print('Adding {}'.format(name))
+  return(dic)
+
+def searchDB(db,dType,search):
+  keys = getDocItems(dType)
+  r = []
+  for o in db:
+    x = ''
+    try:
+      x = db[o]['name']  
+    except:
+      pass
+    if x != '':
+      if (search.lower() in x.lower()) or (x.lower() in search.lower()):
+        #print('Found {} in {}'.format(search,x))
+        r.append(db[o]['_id'])
+  return r
--- runMe.py
+++ runMe.py
@@ -1,13 +1,120 @@
 # This will just use couch.py to connect and list all of the items in the local couchdb
-
 import sys
 import couch
 
+srv = 'http://localhost:5984'
+
+def main():
+  menu()
+
+def viewAll():
+  c = couch.connectSRV(srv)
+  couch.viewAll(c)
+
+def addEvent():
+  c = couch.connectSRV(srv)
+  db = c['event']
+  item = raw_input("Search For Artist: ")
+  f = couch.searchDB(db,'artist',item)
+  count = 0
+  menu = {}
+  for a in f:
+    count += 1
+    menu[str(count)]=db[a]['name']
+  options = menu.keys()
+  options.sort()
+  for entry in options:
+    print entry, menu[entry]
+  selection = raw_input("Please the Artist Number: ")
+  print(menu[selection])
+
+def addDoc(dType):
+  c = couch.connectSRV(srv)
+  db = c['event']
+  if dType != 'crawler':
+    item = raw_input("Type {} Name: ".format(dType))
+    f = couch.searchDB(db,dType,item)
+  else:
+    f = []
+    item = 'crawler'
+  if len(f) < 1:
+    dic = couch.add(dType, item)
+    couch.save(db,dic)
+  else:
+    print('Found the Following Keys:')
+    for a in f:
+      print(a)
+
+
+def menu():
+  menu = {}
+  menu['1']="View Database"
+  menu['2']="View Whats Up"
+  menu['3']="Add Something"
+  menu['4']="Delete Something"
+  menu['0']="Exit"
+  while True:
+    print('\n' * 50)
+    now = couch.now()
+    print("What's Up Tonight? - {}\n".format(now))
+    options = menu.keys()
+    options.sort()
+    for entry in options:
+      print entry, menu[entry]
+    print('\n' * 5)
+    selection = raw_input("Please Select a Number: ")
+    if selection == '1':
+      viewAll()
+    elif selection == '2':
+      print('Stream')
+    elif selection == '3':
+      addMenu()
+    elif selection == '4':
+      print('DeleteMenu')
+    elif selection == '0':
+      break
+    else:
+      print("{} : Please choose a number from list!".format(selection))
+      continue
+    xx = raw_input("Hit Enter to Continue...")
+
+def addMenu():
+  menu = {}
+  menu['1']="Add a venue"
+  menu['2']="Add an artist"
+  menu['3']="Add a region"
+  menu['4']="Add a crawler"
+  menu['5']="Add an agegroup"
+  menu['6']="Add an event"
+  menu['0']="Exit"
+  while True:
+    print('\n' * 50)
+    print("Add Menu\n")
+    options = menu.keys()
+    options.sort()
+    for entry in options:
+      print entry, menu[entry]
+    print('\n' * 5)
+    selection = raw_input("Please Select a Number: ")
+    if selection == '1':
+      addDoc('venue')
+    elif selection == '2':
+      addDoc('artist')
+    elif selection == '3':
+      addDoc('region')
+    elif selection == '4':
+      addDoc('crawler')
+    elif selection == '5':
+      addDoc('ageGroup')
+    elif selection == '6':
+      addEvent()
+    elif selection == '0':
+      break
+    else:
+      print("{} : Please choose a number from list!".format(selection))
+      continue
+    xx = raw_input("Hit Enter to Continue...")
+
 if len(sys.argv) > 1:
   srv = sys.argv[1].lower()
-else:
-  srv = 'http://localhost:5984'
-
-c = couch.connectSRV(srv)
-couch.viewAll(c)
-
+main()
